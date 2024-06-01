6747801b7fb4579f38f38fc7c37f9d817467cd08
sarina <sarinaem75@gmail.com>
2024-06-01 13:32:42 +0330

    create file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import Acordion from './components/Acordion';
+function App() {
+  
+
+  return (
+    <Acordion />
+  )
+}
+
+export default App
---
+++ src/components/Acordion.jsx
@@ -0,0 +1,16 @@
+
+function Acordion() {
+  return (
+    <div className="accordion">
+      <div className="accordion-item">
+        <div className="accordion-item__header">
+          acordion one
+        </div>
+        <div className="accordion-item__content">acordion content</div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Acordion
